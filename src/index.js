f91623109ccb37206a45f70e4012c5e2d8230f3f
bondarkoff <yehor.bondarkoff@gmail.com>
2023-01-16 17:02:22 +0200

    data transformation and randomCharacter component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
 
 import './style/style.scss';
-
-const marveService = new MarvelService();
-
-marveService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)))
 
 ReactDOM.render(
   <React.StrictMode>
